ff84eb45f7e98b35f53538f69a1ea37b0b8553b6
makersy <1352605699@qq.com>
2023-09-06 20:00:58 +0800

    day: 23/09/06

--- src/main/java/com/makersy/leetcode/editor/cn/1123_LowestCommonAncestorOfDeepestLeaves.java
+++ src/main/java/com/makersy/leetcode/editor/cn/1123_LowestCommonAncestorOfDeepestLeaves.java
@@ -83,6 +83,32 @@ class LowestCommonAncestorOfDeepestLeaves{
  * }
  */
 class Solution {
+    class Pair<K, V>{
+        K key;
+        V value;
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
     public TreeNode lcaDeepestLeaves(TreeNode root) {
         return f(root).getKey();
     }

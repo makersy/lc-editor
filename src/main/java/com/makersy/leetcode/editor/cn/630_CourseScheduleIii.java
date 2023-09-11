b4fb52a2abe3b4ba34e2ab42debebeca359e926f
makersy <1352605699@qq.com>
2023-09-11 19:02:49 +0800

    day: 23/09/11

---
+++ src/main/java/com/makersy/leetcode/editor/cn/630_CourseScheduleIii.java
@@ -0,0 +1,77 @@
+/**
+ * 这里有 n 门不同的在线课程，按从 1 到 n 编号。给你一个数组 courses ，其中 courses[i] = [durationi, lastDayi]
+ * 表示第 i 门课将会 持续 上 durationi 天课，并且必须在不晚于 lastDayi 的时候完成。
+ * <p>
+ * 你的学期从第 1 天开始。且不能同时修读两门及两门以上的课程。
+ * <p>
+ * 返回你最多可以修读的课程数目。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：courses = [[100, 200], [200, 1300], [1000, 1250], [2000, 3200]]
+ * 输出：3
+ * 解释：
+ * 这里一共有 4 门课程，但是你最多可以修 3 门：
+ * 首先，修第 1 门课，耗费 100 天，在第 100 天完成，在第 101 天开始下门课。
+ * 第二，修第 3 门课，耗费 1000 天，在第 1100 天完成，在第 1101 天开始下门课程。
+ * 第三，修第 2 门课，耗时 200 天，在第 1300 天完成。
+ * 第 4 门课现在不能修，因为将会在第 3300 天完成它，这已经超出了关闭日期。
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：courses = [[1,2]]
+ * 输出：1
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：courses = [[3,2],[4,3]]
+ * 输出：0
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示:
+ * <p>
+ * <p>
+ * 1 <= courses.length <= 10⁴
+ * 1 <= durationi, lastDayi <= 10⁴
+ * <p>
+ * <p>
+ * Related Topics 贪心 数组 排序 堆（优先队列） 👍 529 👎 0
+ */
+
+package com.makersy.leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+class CourseScheduleIii {
+    public static void main(String[] args) {
+        // Solution solution = new CourseScheduleIii().new Solution();
+    }
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int scheduleCourse(int[][] courses) {
+            Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+            PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> b - a);
+            int sum = 0;
+            for (int[] c : courses) {
+                int d = c[0], e = c[1];
+                sum += d;
+                q.add(d);
+                if (sum > e) sum -= q.poll();
+            }
+            return q.size();
+        }
+    }
+
+    // leetcode submit region end(Prohibit modification and deletion)
+
+}

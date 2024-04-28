121c40177d906e03bcd5b994c86e9ca244289b13
makersy <1352605699@qq.com>
2024-04-28 20:29:24 +0800

    day: 24/04/28

--- src/main/java/com/makersy/leetcode/editor/cn/1017_ConvertToBase2.java
+++ src/main/java/com/makersy/leetcode/editor/cn/1017_ConvertToBase2.java
@@ -50,19 +50,38 @@ class ConvertToBase2 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public String baseNeg2(int n) {
-            if (n == 0 || n == 1) {
-                return String.valueOf(n);
+            if (n == 0) {
+                return "0";
             }
+            int[] bits = new int[32];
+            for (int i = 0; i < 32 && n != 0; i++) {
+                if ((n & 1) != 0) {
+                    bits[i]++;
+                    if ((i & 1) != 0) {
+                        bits[i + 1]++;
+                    }
+                }
+                n >>= 1;
+            }
+            int carry = 0;
+            for (int i = 0; i < 32; i++) {
+                int val = carry + bits[i];
+                bits[i] = val & 1;
+                carry = (val - bits[i]) / (-2);
+            }
+            int pos = 31;
             StringBuilder res = new StringBuilder();
-            while (n != 0) {
-                int remainder = n & 1;
-                res.append(remainder);
-                n -= remainder;
-                n /= -2;
+            while (pos >= 0 && bits[pos] == 0) {
+                pos--;
             }
-            return res.reverse().toString();
+            while (pos >= 0) {
+                res.append(bits[pos]);
+                pos--;
+            }
+            return res.toString();
         }
     }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }

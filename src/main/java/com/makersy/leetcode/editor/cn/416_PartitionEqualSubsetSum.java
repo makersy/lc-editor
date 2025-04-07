43d159b39fbd3be1bce1c4a5252e006d38b17d65
makersy <haoliny123@gmail.com>
2025-04-07 22:13:38 +0800

    day: 25/04/07

---
+++ src/main/java/com/makersy/leetcode/editor/cn/416_PartitionEqualSubsetSum.java
@@ -0,0 +1,77 @@
+/**
+ * 给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：nums = [1,5,11,5] 输出：true 解释：数组可以分割成 [1, 5, 5] 和 [11] 。
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：nums = [1,2,3,5] 输出：false 解释：数组不能分割成两个元素和相等的子集。
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 1 <= nums.length <= 200 1 <= nums[i] <= 100
+ * <p>
+ * <p>
+ * Related Topics 数组 动态规划 👍 2320 👎 0
+ */
+
+package com.makersy.leetcode.editor.cn;
+
+class PartitionEqualSubsetSum {
+
+    public static void main(String[] args) {
+        //Solution solution = new PartitionEqualSubsetSum().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public boolean canPartition(int[] nums) {
+            int n = nums.length;
+            if (n < 2) {
+                return false;
+            }
+            int sum = 0, maxNum = 0;
+            for (int num : nums) {
+                sum += num;
+                maxNum = Math.max(maxNum, num);
+            }
+            if (sum % 2 != 0) {
+                return false;
+            }
+            int target = sum / 2;
+            if (maxNum > target) {
+                return false;
+            }
+            boolean[][] dp = new boolean[n][target + 1];
+            for (int i = 0; i < n; i++) {
+                dp[i][0] = true;
+            }
+            dp[0][nums[0]] = true;
+            for (int i = 1; i < n; i++) {
+                int num = nums[i];
+                for (int j = 1; j <= target; j++) {
+                    if (j >= num) {
+                        dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
+                    } else {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                }
+            }
+            return dp[n - 1][target];
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
